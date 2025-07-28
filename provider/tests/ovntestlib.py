77143d97b2dada826e66f60c890d87ddd8ecfe88
Jasper Berton <jasper.berton@team.blue>
2025-07-28 17:19:35 +0200

    linting final
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- provider/tests/ovntestlib.py
+++ provider/tests/ovntestlib.py
@@ -469,8 +469,12 @@ class PortApiInputMaker(ApiInputMaker):
 
 class SecurityGroupApiInputMaker(ApiInputMaker):
     def __init__(
-            self, name, tenant_id=None, project_id=None, description=None
-        ):
+        self,
+        name,
+        tenant_id=None,
+        project_id=None,
+        description=None
+    ):
         self._name = (SecurityGroupMapper.REST_SEC_GROUP_NAME, name)
         self._description = (
             SecurityGroupMapper.REST_SEC_GROUP_NAME,
