02148d85f38bf71b54562f8df996b6d56c3ded2c
Jasper Berton <jasper.berton@team.blue>
2025-07-30 14:55:30 +0200

    experimentation with extra tags
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- automation/create_it_env.sh
+++ automation/create_it_env.sh
@@ -52,6 +52,8 @@ function start_provider_container {
   kernel_version="$(uname -r)"
   PROVIDER_ID="$(
     ${CONTAINER_CMD} run --privileged -d \
+    --tmpfs /tmp \
+    --tmpfs /run \
 	  -v $PROJECT_ROOT/:$CONTAINER_SRC_CODE_PATH \
 	  -v /lib/modules/$kernel_version:/lib/modules/$kernel_version:ro \
 	  -p 9696:9696 -p 35357:35357 \
